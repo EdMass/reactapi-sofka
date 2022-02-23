238e8363f3cb2b24e8d261c1eff01fc7d832cb01
EdMass <zairmass@gmail.com>
2022-02-23 14:25:56 -0500

    Usar una Api con react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const url = 'https://jsonplaceholder.typicode.com/todos/'
+  
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    console.log(response.status)
+    const responseJson = await response.json()
+    setTodos(responseJson)
+  }
+
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetchApi()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hola mUndo
+      {!todos ? 'Cargando...' :
+        todos.map((todo, index)=>{
+          return <li key={index}>{todo.title} - {todo.completed ? 'OK':'X'}</li>
+        })
+      }
     </div>
   );
 }
